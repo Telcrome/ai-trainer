a4c6c7c45cf39619e0187f3778f8198d63658103
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-01-05 22:09:24 +0100

    Update Alpha version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ai-trainer",
-    version="0.0.2",
+    version="0.0.3",
     author="Raphael Schaefer",
     author_email="raphaelschaefer1@outlook.com",
     description="AI Trainer",
