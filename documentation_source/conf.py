81a6f9790200c013aeeaf5b0218364836740847b
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-02-07 19:37:24 +0100

    Fix sphinx config for ReadTheDocs

--- documentation_source/conf.py
+++ documentation_source/conf.py
@@ -13,11 +13,10 @@
 import os
 import sys
 
-from trainer import VERSION as TRAINER_VERSION
-
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
+from trainer import VERSION as TRAINER_VERSION
 
 # -- Project information -----------------------------------------------------
 
