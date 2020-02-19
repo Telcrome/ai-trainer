d9abf7e31c2e87122d615c653f0350e1bd1bb592
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-02-19 18:48:06 +0100

    Avoid wildcard import

--- trainer/ml/__init__.py
+++ trainer/ml/__init__.py
@@ -6,7 +6,7 @@ The machine learning package bundles everything directly related to learning fro
 For internal models, torch is tightly integrated into trainer.
 """
 try:  # The following modules depend on torch, for CI systems without torch a try block is required
-    from trainer.ml.utils import *
+    from trainer.ml.utils import distance_transformed, im_one_hot, pair_augmentation, normalize_im
     from trainer.ml.visualization import VisBoard, LogWriter, logger
     from trainer.ml.losses import dice_loss, FocalLoss
     from trainer.ml.layers import ConvGRUCell, ConvGRU
