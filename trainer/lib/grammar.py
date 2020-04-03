126ab8e1cc8570a3d3e906635c68509c57b03d31
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-04-03 12:30:28 +0200

    Simplify specifying grammar semantics using DslSemantics

--- trainer/lib/grammar.py
+++ trainer/lib/grammar.py
@@ -6,7 +6,7 @@ import itertools
 import infinite
 import random
 from abc import ABC, abstractmethod
-from typing import get_type_hints, Optional, Dict, List, Union, Tuple, Callable, Generator
+from typing import get_type_hints, Optional, Dict, List, Union, Tuple, Callable, Generator, Any
 from functools import reduce
 
 import numpy as np
@@ -116,110 +116,27 @@ def dsl_func(priority: float):
     return wrapper
 
 
-class DomainLang(ABC):
+class DslSemantics(ABC):
+    """
+    Defines execution context and utility functionality for running python statements from strings.
 
-    def __init__(self, start_symbol: type, max_resources=10000):
-        self.start_symbol = start_symbol
-        self.state = {}
-        self.fs: List[Tuple[Callable, float]] = []
-        self.constants: List[Enum] = []
-        self.max_resources = max_resources
-        self.resources = 0
+    Child classes define methods that can be called inside of the DSL constructs.
+    """
 
-    def set_state(self, state):
+    def __init__(self, max_resources=10000):
+        self.resources, self.max_resources, self.state = 0, max_resources, {}
+        self.fs: Dict[str, Any] = {}
+
+    def execute_program(self, prog: str, state: Dict):
         self.state = state
-
-    def execute_program(self, prog: Tuple[Callable, List], state: Dict):
         self.resources = self.max_resources
-        self._execute_program(prog)
-
-    def _execute_program(self, p: Tuple[Callable, List]) -> Optional[type]:
-        self.resources -= 1
-        if self.resources <= 0:
-            return None
-        params = p[1]
-        if not params:
-            return p[0]()
-        parameters = [self._execute_program(param) for param in params]
-        if None in parameters:
-            return None
-        return p[0](*parameters)
-
-    def get_prog_repr(self, p: Tuple[Callable, List], depth=1) -> str:
-        if p[1]:
-            tabs = "  ".join(['' for _ in range(depth + 1)])
-            param_repr = f'\n{tabs}'.join([''] + [self.get_prog_repr(param, depth + 1) for param in p[1]])
-        else:
-            param_repr = ''
-        return f'{get_func_name(p[0])}{param_repr}'
-
-
-class Dsl:
-    def __init__(self, lang: DomainLang):
-        self.lang: DomainLang = lang
-        self.prod_rules: Dict[type, List[Tuple[Callable, float]]] = {}
-
-        for f, score in lang.fs:
-            # args, ret = analyse_function_type(f)
-            self.add_rule(f, score)
-
-            # args, ret = analyse_function_type(f)
-            # fulltype = Callable[[*args], ret]
-            #
-            # def f_wrapper() -> fulltype:
-            #     return f
-            #
-            # self.add_rule(f_wrapper, score)
-        for c in lang.constants:
-            for c_val in c:
-                def func_wrapper() -> c:
-                    return c_val
-
-                self.add_rule(func_wrapper, 1.)
-
-    def add_rule(self, f: Callable, score: float):
-        args, ret = analyse_function_type(f)
-        # fulltype = Callable[[*args], ret]
-
-        if ret not in self.prod_rules:
-            self.prod_rules[ret] = []
-        # if fulltype not in self.prod_rules:
-        #     self.prod_rules[fulltype] = []
-
-        self.prod_rules[ret].append((f, score))
-        # self.prod_rules[fulltype].append((f, score))
-
-    def __repr__(self):
-        res = ""
-        for prod_rule_key in self.prod_rules:
-            right_repr = ''
-            for substitution in self.prod_rules[prod_rule_key]:
-                prod_abbreviation = f_to_str(substitution[0])
-                right_repr += f" {prod_abbreviation} ({substitution[1]}) | "
-            res += f'{prod_rule_key} -> {right_repr[:-3]}\n'
+        try:
+            res = eval(
+                prog,
+                self.fs,
+                self.state
+            )
+        except Exception as e:
+            print(f'Tried to execute {prog}')
+            print(Exception)
         return res
-
-    def read_program(self) -> Generator:
-        for item in self._read_symbol(0, self.lang.start_symbol):
-            yield item
-
-    def _read_symbol(self, depth: int, sym: type) -> Generator:
-        rules, probas = [], []
-        for substitution, p in self.prod_rules[sym]:
-            rules.append(substitution)
-            probas.append(p)
-
-        rule_gens = []
-        for rule in rules:
-            param_syms = analyse_function_type(rule)[0]
-            if not param_syms:
-                rule_gens.append((rule, []))
-            else:
-                sym_gens = [self._read_symbol(depth + 1, param_sym) for param_sym in param_syms]
-                g = prepend_gen(rule, lib.product(sym_gens))
-                rule_gens.append(g)
-
-        for random_rule_gen in lib.sample_randomly(rule_gens, probas):
-            # yield reduce(lambda x, y: x + y, [i for i in random_rule_gen])
-
-            yield random_rule_gen
