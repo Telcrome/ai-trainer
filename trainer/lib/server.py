32df215910648f8ef22530b6ed05ac5e56accd59
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-04-14 12:08:31 +0200

    Add mockup logs endpoint

--- trainer/lib/server.py
+++ trainer/lib/server.py
@@ -1,4 +1,7 @@
+import datetime
 from flask import Flask, jsonify
+
+import numpy as np
 
 app = Flask(__name__)
 
@@ -6,6 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({'Description': 'Main Page of the trainer web gui'})
+
+
+@app.route('/logs/', methods=['GET'])
+def get_logs():
+    res = [
+        {'time': datetime.datetime.now(), 'title': 'plot1', 'type': 'text', 'payload': "Hello"},
+        {'time': datetime.datetime.now(), 'title': 'plot1', 'type': 'arr', 'payload': '/big_bin/id1'}
+    ]
+    return jsonify(res)
 
 
 if __name__ == '__main__':
