7d3762e19a11d0d41cb30a23ad95aede4b9dd84f
raphael <jan.raphael.schaefer@rwth-aachen.de>
2020-05-31 21:45:13 +0200

    Start working on flexx logger GUI

--- tutorials/simple_flexx.py
+++ tutorials/simple_flexx.py
@@ -1,7 +1,12 @@
 import os
-from flexx import app, ui, flx
+from flexx import flx
+
 
 class Example(flx.PyWidget):
+
+    def __init__(self):
+        self.button, self.lbl = None, None
+        super().__init__()
 
     def init(self):
         self.button = flx.Button(text='hello')
@@ -9,12 +14,9 @@ class Example(flx.PyWidget):
 
     @flx.reaction('button.pointer_click')
     def some_stuff(self, *events):
-        # with ui.TreeWidget(max_selected=2):
-
-        #     for t in ['foo', 'bar', 'spam', 'eggs']:
-        #         ui.TreeItem(text=t, checked=True)
-        # self.lbl.set_html('1<br/>2')
         self.lbl.set_html('<br />'.join(os.listdir('.')))
+        print(events)
+
 
 app = flx.App(Example)
 app.export('example.html', link=0)
