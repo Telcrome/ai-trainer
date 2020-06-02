4c3f7054a5df18a3aef4c99aaace305afbd47a2c
Telcrome <raphaelschaefer1@outlook.com>
2020-06-02 13:10:08 +0200

    Start working on computational graphs package

---
+++ trainer/cg/__init__.py
@@ -0,0 +1,6 @@
+"""
+Computational graphs package, contributes:
+
+- A greedy, decision tree based, image transformation module
+- A module for defining computational graphs using general python functions, trained using MCMC
+"""
