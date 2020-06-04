704c46a22f7ab0cbd0a6d345194c2f59ff2b278a
Telcrome <raphaelschaefer1@outlook.com>
2020-06-04 13:32:46 +0200

    Move visualization webgui into separate package

--- trainer/lib/visualization.py
+++ trainer/lib/visualization.py
@@ -3,41 +3,12 @@ from __future__ import annotations
 from typing import Any, List, Optional
 import logging
 import os
-from flexx import flx
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from bokeh.plotting import figure
 
-# import trainer.lib as lib
-
-x = np.linspace(0, 6, 50)
-p1 = figure()
-p1.line(x, np.sin(x))
-
-p2 = figure()
-p2.line(x, np.cos(x))
-
-
-class DebuggerGui(flx.PyWidget):
-
-    # def __init__(self):
-    #     # self.bs: List[Optional[flx.Button]] = [None, None, None]
-    #     super().__init__()
-
-    def init(self):
-        with flx.HBox():
-            with flx.VBox():
-                self.bs0 = flx.Button(text='Button1', flex=0)
-                self.bs1 = flx.Button(text='Button2', flex=1)
-                self.bs2 = flx.Button(text='Button3', flex=2)
-                self.prog = flx.ProgressBar(flex=1, value=0.1, text='{percent} done')
-                self.lbl_placeholder = flx.Label(flex=1, style='overflow-y: scroll;')
-            with flx.VBox():
-                self.lbl = flx.Label(flex=1, style='overflow-y: scroll;')
-                # flx.BokehWidget.from_plot(p1)
-                # flx.BokehWidget.from_plot(p2)
+import trainer.lib as lib
 
 
 class LogWriter:
@@ -124,9 +95,4 @@ class LogWriter:
         return self._get_run_path()
 
 
-logger = flx.App(DebuggerGui)
-logger.export('logger.html', link=0)
-
-if __name__ == '__main__':
-    logger.launch('browser')
-    flx.run()
+logger = LogWriter()
