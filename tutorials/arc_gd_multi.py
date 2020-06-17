2a5da3bcfbe1209ada5febce887f0baa65ea0a39
Telcrome <raphaelschaefer1@outlook.com>
2020-06-17 18:50:59 +0200

    Plot prediction after every task optimization

--- tutorials/arc_gd_multi.py
+++ tutorials/arc_gd_multi.py
@@ -85,36 +85,47 @@ if __name__ == '__main__':
     # train_loader = train_set.get_torch_dataloader(batch_size=BATCH_SIZE)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(task_network.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(
+        task_network.parameters(),
+        lr=5e-2
+    )
+    # loop over all training tasks in each epoch
+    for epoch in range(EPOCHS):
 
-    for epoch in range(EPOCHS):  # loop over all training tasks in each epoch
-
-        running_loss = 0.0
+        # Looping over all training tasks
         for i, task in enumerate(train_set, 0):
             train_pairs, test_pairs, s_name = task
             # Try to improve on the task specific network
             # TODO: Do not iterate through all training examples, this favors tasks with bigger datasets
-            for situation, target in train_pairs:
-                situation = torch.from_numpy(situation).float().unsqueeze(0)
-                target = torch.from_numpy(target).long().unsqueeze(0)
+            for _ in range(EPOCHS):
+                for situation, target in train_pairs:
+                    situation = torch.from_numpy(situation).float().unsqueeze(0)
+                    target = torch.from_numpy(target).long().unsqueeze(0)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-                # forward + backward + optimize
-                outputs = task_network(situation)
-                loss = criterion(outputs, target)
-                loss.backward()
-                optimizer.step()
+                    # forward + backward + optimize
+                    outputs = task_network(situation)
+                    loss = criterion(outputs, target)
+                    loss.backward()
+                    optimizer.step()
 
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+            # Temporary check to visualize the last training example
+            pred_img = ml.one_hot_to_cont(outputs.detach().squeeze().numpy())
+            lib.logger.debug_var(pred_img)
 
             # Logging and statistics of the performance of the network corresponding to task
             # TODO
+            for situation, target in test_pairs:
+                situation = torch.from_numpy(situation).float().unsqueeze(0)
+                target = torch.from_numpy(target).long().unsqueeze(0)
+
+                with torch.no_grad():
+                    prediction = task_network(situation)
+
+                pred_img = ml.one_hot_to_cont(prediction.squeeze().numpy())
+                lib.logger.debug_var(pred_img)
+                # print(prediction)
 
     print('Finished Training')
